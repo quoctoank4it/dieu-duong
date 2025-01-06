d06b30e4b0ed517b913c87e2e01abe50515a7974
quoctoank4it <quoctoank4it@gmail.com>
2025-01-06 21:59:12 +0700

    edit file

--- src/Components/ChuyenDe4/D11_0_ChanThuongSoNao/index.js
+++ src/Components/ChuyenDe4/D11_0_ChanThuongSoNao/index.js
@@ -12,7 +12,44 @@ const D11_0_ChanThuongSoNao = ({}) => {
             justifyContent: "center",
             marginTop: 10,
           }}
-        ></div>
+        >
+          <div className="card" style={{ width: "100%" }}>
+            <ul>
+              <li style={{ listStyle: "none", fontWeight: "bold" }}>
+                Sơ cứu khi nạn nhân tỉnh
+              </li>
+              <li style={{ listStyle: "none", paddingLeft: 10 }}>
+                Khuyến khích người bị thương giảm thiểu mọi cử động ở đầu hoặc
+                cổ.
+              </li>
+              <li style={{ listStyle: "none", paddingLeft: 10 }}>
+                Vết thương trên đầu có thể chảy máu nhiều, trong trường hợp đó
+                cần cầm máu vết thương bằng cách ấn trực tiếp và băng. Trong khi
+                kiểm tra vết thương, tránh nhầm với cục máu đông hình thành
+                trong tóc. Trấn an nạn nhân và cố gắng giữ cho họ bình tĩnh.
+              </li>
+              <li style={{ listStyle: "none", fontWeight: "bold" }}>
+                Sơ cứu khi nạn nhân bất tỉnh
+              </li>
+              <li style={{ listStyle: "none", paddingLeft: 10 }}>
+                Không nên di chuyển nạn nhân trừ khi họ đang ở trong tình trạng
+                nguy cấp. Mọi sự di chuyển không cần thiết đều có thể gây ra các
+                biến chứng lớn hơn cho chính vết thương sọ não, cột sống hay
+                những vết thương liên quan khác. Một nguyên tắc là nếu đầu bị
+                thương thì cổ cũng có thể bị tình trạng tương tự.
+              </li>
+              <li style={{ listStyle: "none", paddingLeft: 10 }}>
+                Vai trò của bạn là bảo vệ nạn nhân khỏi mọi nguy hiểm tiềm ẩn
+                tại hiện trường. Bạn cũng nên theo dõi đường thở và hô hấp của
+                nạn nhân cho đến khi xe cứu thương tới. Nếu người bị thương thở
+                yếu đi do có vấn đề với đường hô hấp, có thể cần rất thận trọng
+                để ngửa đầu họ ra sau (và nâng đỡ) cho đến khi nhịp thở trở lại
+                bình thường. Nếu nạn nhân ngừng thở hoặc không bắt được mạch, có
+                thể cần hồi sức tim phổi (CPR).
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
